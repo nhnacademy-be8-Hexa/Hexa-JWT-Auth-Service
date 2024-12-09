7e65740a21971a67718fe31a672706b97652f551
조승주 <tmdwn3318@gmail.com>
2024-12-09 09:36:01 +0900

    init

---
+++ src/test/java/com/nhnacademy/hexajwtauthservice/HexaJwtAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.hexajwtauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaJwtAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
