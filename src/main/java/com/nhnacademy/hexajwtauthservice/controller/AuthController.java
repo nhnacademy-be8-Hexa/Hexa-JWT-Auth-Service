c05f7d856a3fdbc8db98ef3bd974941b9975f0ac
조승주 <tmdwn3318@gmail.com>
2024-12-10 15:50:34 +0900

    http로 로그인 비밀번호 일치 검사 테스트

--- src/main/java/com/nhnacademy/hexajwtauthservice/controller/AuthController.java
+++ src/main/java/com/nhnacademy/hexajwtauthservice/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.nhnacademy.hexajwtauthservice.controller;
 
+import com.nhnacademy.hexajwtauthservice.adapter.MemberAdapter;
 import com.nhnacademy.hexajwtauthservice.domain.Member;
 import com.nhnacademy.hexajwtauthservice.domain.MemberStatus;
 import com.nhnacademy.hexajwtauthservice.domain.Rating;
 import com.nhnacademy.hexajwtauthservice.domain.Role;
 import com.nhnacademy.hexajwtauthservice.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,15 +18,32 @@ import java.security.Principal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth/members")
 public class AuthController {
+
+    private final MemberAdapter memberAdapter;
 
     @GetMapping
     public ResponseEntity<UserResponse> getMember(
             @RequestHeader("X-USER-ID") String userId,
             Principal principal
     ){
+//        Member member = memberAdapter.getMemberById(userId);
+//        UserResponse response = new UserResponse(
+//                member.getMemberId(),
+//                member.getMemberPassword(),
+//                member.getMemberName(),
+//                member.getMemberNumber(),
+//                member.getMemberBirthAt(),
+//                member.getMemberCreatedAt(),
+//                member.getMemberLastLoginAt(),
+//                member.getMemberRole().toString(),
+//                member.getRating().getRatingId().toString(),
+//                member.getMemberStatus().getStatusId().toString()
+//        );
+
 
         return ResponseEntity.ok(
                 new UserResponse("testMember",
--- src/main/java/com/nhnacademy/hexajwtauthservice/security/details/CustomUserDetailsService.java
+++ src/main/java/com/nhnacademy/hexajwtauthservice/security/details/CustomUserDetailsService.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -26,18 +28,35 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // db에서 유저 가져오기 (페인 클라 어댑터)
 //        return new PrincipalDetails(memberAdapter.getMemberById(username));
+
+        // 테스트용 급조 db
+        Map<String, Object> map = new HashMap<>();
+        map.put("testMember", new Member("testMember",
+                new BCryptPasswordEncoder().encode("1234"),
+                "test name",
+                "01012345678",
+                LocalDate.now(),
+                LocalDate.now(),
+                LocalDateTime.now(),
+                Role.MEMBER,
+                new Rating(1L, "gold", 20),
+                new MemberStatus(1L, "online")
+        ));
+
+        map.put("testMember2", new Member("testMember2",
+                new BCryptPasswordEncoder().encode("12345"),
+                "test name",
+                "01012345678",
+                LocalDate.now(),
+                LocalDate.now(),
+                LocalDateTime.now(),
+                Role.MEMBER,
+                new Rating(1L, "gold", 20),
+                new MemberStatus(1L, "online")
+        ));
+
         return new PrincipalDetails(
-                new Member("testMember",
-                        new BCryptPasswordEncoder().encode("1234"),
-                        "test name",
-                        "01012345678",
-                        LocalDate.now(),
-                        LocalDate.now(),
-                        LocalDateTime.now(),
-                        Role.MEMBER,
-                        new Rating(1L, "gold", 20),
-                        new MemberStatus(1L, "online")
-                        )
+                (Member) map.get(username)
         );
     }
 }
