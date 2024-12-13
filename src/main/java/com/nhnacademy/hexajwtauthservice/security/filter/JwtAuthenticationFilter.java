a004cce8b81f6c2457c727e739eb61c7bb4c4d99
조승주 <tmdwn3318@gmail.com>
2024-12-13 14:36:39 +0900

    fix: 로그인 실패 시 exception 발생하는 대신 null을 반환해주도록 변경

--- src/main/java/com/nhnacademy/hexajwtauthservice/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/nhnacademy/hexajwtauthservice/security/filter/JwtAuthenticationFilter.java
@@ -47,15 +47,31 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             throw new RuntimeException(e);
         }
 
-        log.debug("JwtAuthenticationFilter : {}", loginRequest);
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getId(),loginRequest.getPassword());
-        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
 
-        return authentication;
+        try {
+            // 인증 요청 생성
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(loginRequest.getId(), loginRequest.getPassword());
+
+            // 인증 처리
+            return authenticationManager.authenticate(authenticationToken);
+
+        } catch (AuthenticationException | NullPointerException e) {
+            // 인증 실패 시 null 응답 처리
+            return null;
+        }
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        if(authResult == null) {
+            // 인증 실패로 프론트에 null 전달
+            PrintWriter writer = response.getWriter();
+            writer.write("null"); // 프론트에 null 응답
+            writer.close();
+            return;
+        }
 
         PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
         Calendar calendar = Calendar.getInstance();
@@ -66,7 +82,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .map(GrantedAuthority::getAuthority) // 권한 이름만 추출
                 .findFirst() // 첫 번째 요소 가져오기
                 .orElse(null); // 없으면 null
-
 
         String jwtToken = Jwts.builder()
                 .setHeaderParam("typ", jwtProperties.getTokenPrefix())
@@ -80,6 +95,5 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         PrintWriter printWriter = response.getWriter();
         printWriter.write(jwtToken);
         printWriter.close();
-
     }
 }
