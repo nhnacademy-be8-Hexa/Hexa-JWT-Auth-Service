32eb2edabc7feba892658da2f8aa8e75465bb614
조승주 <tmdwn3318@gmail.com>
2024-12-13 15:45:55 +0900

    test: ApplicationStatusControllerTest

---
+++ src/test/java/com/nhnacademy/hexajwtauthservice/controller/ApplicationStatusControllerTest.java
@@ -0,0 +1,48 @@
+package com.nhnacademy.hexajwtauthservice.controller;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.InstanceInfo;
+import com.nhnacademy.hexajwtauthservice.actuator.ApplicationStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc(addFilters = false) // Security 필터 비활성화
+@WebMvcTest(ApplicationStatusController.class)
+class ApplicationStatusControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ApplicationInfoManager applicationInfoManager;
+
+    @MockBean
+    private ApplicationStatus applicationStatus;
+
+    @Test
+    void testStopStatus() throws Exception {
+        mockMvc.perform(post("/actuator/status/down"))
+                .andExpect(status().isOk());
+
+        verify(applicationInfoManager).setInstanceStatus(InstanceInfo.InstanceStatus.DOWN);
+        verify(applicationStatus).stopService();
+    }
+
+    @Test
+    void testStartStatus() throws Exception {
+        mockMvc.perform(post("/actuator/status/up"))
+                .andExpect(status().isOk());
+
+        verify(applicationInfoManager).setInstanceStatus(InstanceInfo.InstanceStatus.UP);
+        verify(applicationStatus).startService();
+    }
+}
