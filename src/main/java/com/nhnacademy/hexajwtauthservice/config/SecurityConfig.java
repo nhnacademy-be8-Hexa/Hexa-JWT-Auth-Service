40f6c50c4c2556c2046dc28acd4d19272a0777d3
Que-Sera-Sera-Today <30decade@gmail.com>
2025-01-02 18:23:00 +0900

    controller로 jwt 로그인 처리하게 바꿈

--- src/main/java/com/nhnacademy/hexajwtauthservice/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/hexajwtauthservice/config/SecurityConfig.java
@@ -3,23 +3,19 @@ package com.nhnacademy.hexajwtauthservice.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.hexajwtauthservice.adapter.MemberAdapter;
 import com.nhnacademy.hexajwtauthservice.properties.JwtProperties;
-import com.nhnacademy.hexajwtauthservice.security.details.CustomUserDetailsService;
-import com.nhnacademy.hexajwtauthservice.security.filter.JwtAuthenticationFilter;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
@@ -40,8 +36,6 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable);
         http
                 .httpBasic(AbstractHttpConfigurer::disable);
-        http
-                .addFilterAt(new JwtAuthenticationFilter(authenticationManager(null),jwtProperties,objectMapper), UsernamePasswordAuthenticationFilter.class);
 
         http
                 .authorizeHttpRequests(auth->auth.anyRequest().permitAll());
@@ -49,24 +43,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
-    @Bean
-    public CustomUserDetailsService customUserDetailsService() {
-        return new CustomUserDetailsService(memberAdapter);
-    }
-
-    // 패스워드 인코더 BCryptEncoder 사용
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService());
-        return daoAuthenticationProvider;
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
